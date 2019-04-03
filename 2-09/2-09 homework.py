2f0e5f549a900a4fa098933cf7b04a2e9196fbf6
Artem Pakhomov <45041550+apisourse@users.noreply.github.com>
2019-04-03 17:44:06 +0300

    save

---
+++ 2-09/2-09 homework.py
@@ -0,0 +1,28 @@
+import csv
+from pymongo import MongoClient
+
+client = MongoClient
+concerts_db = client.['concerts_db']
+
+def read_csv(filename):
+    pass
+
+def add_artist():
+    pass
+
+def add_place():
+    pass
+
+def add_price():
+    pass
+
+def add_date():
+    pass
+
+
+
+def run():
+    pass
+
+if __name__ == '__main__':
+    run()
