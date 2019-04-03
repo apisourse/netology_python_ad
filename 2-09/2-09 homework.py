a1484f809b54e8b201815b9fe12da35ec1df06f3
Artem Pakhomov <45041550+apisourse@users.noreply.github.com>
2019-04-03 18:21:43 +0300

    save

--- 2-09/2-09 homework.py
+++ 2-09/2-09 homework.py
@@ -2,28 +2,40 @@ import csv
 from pymongo import MongoClient
 
 client = MongoClient()
-concerts_db = client['concerts_db']
+db = client['concerts_db']
+
 
 def read_csv(filename):
     pass
 
-def add_artist():
-    pass
+
+def add_artist(artist):
+    is_exists = db.artists.find_one({'name': artist})
+    if is_exists:
+        print(f'{artist} уже существует')
+        return is_exists['_id']
+
+    res = db.artist.insert_one({'name': artist})
+    print(f'{artist} добавлен c {res["_id"]}')
+    return res.inserted_id
+
 
 def add_place():
     pass
 
+
 def add_price():
     pass
+
 
 def add_date():
     pass
 
 
-
 def run():
     filename = 'artists.csv'
     pass
+
 
 if __name__ == '__main__':
     run()
